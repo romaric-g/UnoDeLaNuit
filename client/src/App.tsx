4784d1e5f764c53dcbdc810d7e5b0c831b45affe
antoinetardivel <tardivelantoine@gmail.com>
2020-11-21 03:08:48 +0100

    début front + pages + atomize

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,81 @@
 import React from 'react';
 import { io } from 'socket.io-client';
 import './App.css';
-import Jeu from './components/Jeu/Jeu'
+import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+import Home from './pages/Home/Home'
+import Lobby from './pages/Lobby/Lobby'
+import Room from './pages/Room/Room'
+import Game from './pages/Game/Game'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+var { ThemeProvider } = require('atomize');
+var { StyleReset } = require('atomize');
 
 const socket = io('http://localhost:4000');
+
+const debug = process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+const engine = new Styletron();
 
 socket.emit('play')
 socket.on('reverseplay', () => {
   console.log('reverseplay')
 })
 
+const theme = {
+  colors: {
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+};
+
 function App() {
   return (
-    <div className="App">
-      <Jeu />
-    </div>
+    <Router>
+      <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+        <ThemeProvider theme={theme}>
+          <StyleReset />
+          <div className="App">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/Room">Room</Link>
+                </li>
+                <li>
+                  <Link to="/Lobby">Lobby</Link>
+                </li>
+                <li>
+                  <Link to="/Game">Game</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <Switch>
+            <Route path="/Room">
+              <Room />
+            </Route>
+            <Route path="/Lobby">
+              <Lobby />
+            </Route>
+            <Route path="/Game">
+              <Game />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </ThemeProvider>
+      </StyletronProvider>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Lobby/Lobby.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Lobby() {
+    return (
+        <div className="Container">
+            <h1>Lobby</h1>
+        </div>
+    );
+}
+export default Lobby;
